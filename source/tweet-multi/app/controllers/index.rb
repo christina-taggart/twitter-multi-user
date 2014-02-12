a21cb76dadc284bfe93822bd4f733f57f7fac86a
Alexander Leishman <null>
2014-02-12 11:33:49 -0800

    Tweet on behalf of user working

--- source/tweet-multi/app/controllers/index.rb
+++ source/tweet-multi/app/controllers/index.rb
@@ -17,9 +17,22 @@ get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-
+  @user = User.find_or_create_by_oauth_token(@access_token.token)
+  @user.update_attributes(oauth_secret: @access_token.secret)
+  session[:user_id] = @user.id
   # at this point in the code is where you'll need to create your user account and store the access token
 
   erb :index
+end
 
+post '/tweet' do
+  @user = User.find(session[:user_id])
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_KEY']
+    config.consumer_secret     = ENV['TWITTER_SECRET']
+    config.access_token        = @user.oauth_token
+    config.access_token_secret = @user.oauth_secret
+  end
+  client.update(params[:tweet])
+  redirect '/'
 end
