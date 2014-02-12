59c7b21fe6b6e0ec6dbdb0e3c6d7b0d2d952427e
Patrick Vilhena & Eli Shkurkin <pair+nifengle.shkurkin@devbootcamp.com>
2014-02-12 10:37:54 -0800

    Release 0

--- source/tweet-multi/config/environment.rb
+++ source/tweet-multi/config/environment.rb
@@ -22,7 +22,7 @@ require 'erb'
 
 require 'oauth'
 require 'twitter'
-
+require 'yaml'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
@@ -47,3 +47,9 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+
+#API keys
+keys = YAML.load_file(APP_ROOT.join('config', 'keys.yml'))
+keys.each{|k,v| ENV[k]=v}
+
