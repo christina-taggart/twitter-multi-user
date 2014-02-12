92ec8cac181288f000260727880c7438ca00f0b4
Darcey Lachtman & John Olmsted <pair+dslachtman.qsymmachus@devbootcamp.com>
2014-02-12 11:25:24 -0800

    User is now stored in local DB after get '/auth'

--- source/tweet-multi/app/controllers/index.rb
+++ source/tweet-multi/app/controllers/index.rb
@@ -17,13 +17,14 @@ get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-
   # at this point in the code is where you'll need to create your user account and store the access token
-
+  username = @access_token.params[:screen_name]
+  oauth_token = @access_token.params[:oauth_token]
+  oauth_secret = @access_token.params[:oath_token_secret]
+  User.create(username: username, oauth_token: oauth_token, oauth_secret: oauth_secret)
   erb :index
-
 end
 
 error do
-  "Oh crap something went wrong – " + response.env[sinatra.error].message
+  "Oh crap something went wrong - " + response.env[sinatra.error].message
 end
