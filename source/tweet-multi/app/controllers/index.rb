23d41175c0858af80a3c488636c8071ffdd8eabe
David Goodman & Emmanuel Kaunitz <pair+dgoodman1224.mechamanny@devbootcamp.com>
2014-02-12 17:19:46 -0800

    Tweet functionality works oauth is confusing

--- source/tweet-multi/app/controllers/index.rb
+++ source/tweet-multi/app/controllers/index.rb
@@ -1,6 +1,16 @@
 before do
-  @user = User.find(session[:id]) if session[:id]
+  if session[:id]
+    @user = User.find(session[:id])
+    @access_token = session[:access_token]
+    $client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_SECRET"]
+      config.access_token        = @user.oauth_token
+      config.access_token_secret = @user.oauth_secret
+    end
   end
+
+end
 
 get '/' do
   erb :index
@@ -20,25 +30,21 @@ end
 get '/auth' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+  session[:access_token] = @access_token
 
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
   # at this point in the code is where you'll need to create your user account and store the access token
-   @user = User.create(username: @access_token.params[:screen_name], oauth_token: params[:oauth_token], oauth_secret: params[:oauth_verifier])
+   @user = User.create(username: @access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret)
    session[:id] = @user.id
   erb :index
 
 end
 
-post '/' do
+post '/tweet_something' do
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['TWITTER_KEY']
-  config.consumer_secret     = ENV['TWITTER_SECRET']
-  config.access_token        = @user.oauth_token
-  config.access_token_secret = @user.oauth_secret
-end
 
-client.update(params[:tweet])
+$client.update(params[:tweet_text])
+erb :index
 
 end
