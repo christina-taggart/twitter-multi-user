be9481a506dd98da2fa9067ca32b323f40cfaaea
Apprentice <apprentice@dbc12.local>
2014-02-12 17:42:03 -0800

    Tweet with oAuth

--- source/tweet-multi/app/controllers/index.rb
+++ source/tweet-multi/app/controllers/index.rb
@@ -1,3 +1,5 @@
+require 'twitter'
+
 get '/' do
   erb :index
 end
@@ -15,11 +17,35 @@ end
 get '/auth' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+  session[:access_token] = @access_token
+  p "*"*200
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
 
-  # at this point in the code is where you'll need to create your user account and store the access token
+  @user = User.find_or_create_by_oauth_token(@access_token.token)
+  @user.oauth_secret = @access_token.secret
+  @user.username = @access_token.params[:screen_name]
+  @user.save
+
+
+
 
   erb :index
 
 end
+
+post '/tweet' do
+  @access_token = session[:access_token]
+
+  @user = User.find_by_oauth_token(@access_token.token)
+  $client = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV['TWITTER_KEY']
+    config.consumer_secret = ENV['TWITTER_SECRET']
+    config.access_token = @user.oauth_token
+    config.access_token_secret = @user.oauth_secret
+  end
+
+  $client.update(params[:tweet_text])
+  redirect '/'
+end
+
